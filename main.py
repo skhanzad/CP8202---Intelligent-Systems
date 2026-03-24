47993dbc90e5ea8694bff9001d7c075ad3e1ddbb
TheoRusu <theorusu11@gmail.com>
2026-03-24 12:49:36 -0400

    Start of knowledge graph

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
+from email import message
+from enum import Enum
+from operator import ne
 from typing import TypedDict
 from langgraph.graph import StateGraph, START, END
 from langchain_core.messages import HumanMessage, BaseMessage
 from langchain_ollama import ChatOllama
 
-class AgentState(TypedDict):
+class MemoryType(Enum):
+    EPISODIC = "episodic"
+    SEMANTIC = "semantic"
+
+MEMORY_INFERENCE_PROMPT = """Classify the following user message into episode memory, semantic memory, both, or neither:"""
+
+class ChatbotState(TypedDict):
     knowledge_graph: dict
     messages: list[BaseMessage]
 
-llm = ChatOllama(model="gemma2")
+chatbot_llm = ChatOllama(model="gemma2")
+inference_llm = ChatOllama(model="gemma2")
 
-def chatbot(state: AgentState) -> AgentState:
-    response = llm.invoke(state["messages"])
+def chatbot(state: ChatbotState) -> ChatbotState:
+    response = chatbot_llm.invoke(state["messages"])
     return {"messages": state["messages"] + [response]}
 
+
+def infer_memory_types(state: ChatbotState) -> ChatbotState:
+    last_human_message = next((msg for msg in reversed(state["messages"]) if isinstance(msg, HumanMessage)), None)
+    
+    prompt = MEMORY_INFERENCE_PROMPT.format(message=last_human_message.content)
+
+    response = inference_llm.invoke([HumanMessage(content=prompt)])
+
+    raw = response.content.strip().lower()
+
+    memory_type = MemoryType(raw) if raw in MemoryType._value2member_map_ else None
+
+    return {**state, "memory_type": memory_type}
+
+def add_to_knowledge_graph(state: ChatbotState) -> ChatbotState:
+    updated_kg = state["knowledge_graph"]
+    return {"messages": state["messages"], "knowledge_graph": updated_kg}
+
+
 graph = (
-    StateGraph(AgentState)
+    StateGraph(ChatbotState)
     .add_node("node", chatbot)
+    .add_node
     .add_edge(START, "node")
     .add_edge("node", END)
 )
